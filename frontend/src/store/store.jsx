94213dba74050c94c9fcae38c39c855ecc1a2ef5
alexion10 <36454271+alexion10@users.noreply.github.com>
2022-10-31 10:10:40 +0200

    refactoring code

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,12 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { usersApi, usersTest } from './api';
-import user  from './userCred';
+import { usersApi } from './api';
 
 export const store = configureStore({
   reducer: {
-    [usersApi.reducerPath]: usersApi.reducer,
-    user
+    [usersApi.reducerPath]: usersApi.reducer
   },
   
   middleware: (getDefaultMiddleware) => 
